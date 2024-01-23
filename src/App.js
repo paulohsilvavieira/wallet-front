0a582c6030bc19089d40d368ca446fdb8ec0ac5d
Paulo Henrique <paulo.hvieira@contavoltz.com>
2024-01-23 17:30:36 -0300

    create view

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import { Card, Col, Form, Button, Container, Row } from "react-bootstrap";
 function App() {
+  const [formData, setFormData] = useState({});
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container style={{ paddingTop: 10 }}>
+      <Row>
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Title>Create a transfer</Card.Title>
+              <Form>
+                <Form.Group
+                  className="mb-3"
+                  controlId="exampleForm.ControlInput1"
+                >
+                  <Form.Label>Address BTC</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="address wallet"
+                    value={formData.address}
+                    onChange={(e) =>
+                      setFormData({
+                        ...formData,
+                        address: e.target.value,
+                      })
+                    }
+                  />
+                </Form.Group>
+                <Form.Group
+                  className="mb-3"
+                  controlId="exampleForm.ControlTextarea1"
+                >
+                  <Form.Label>Amount</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="0.000001"
+                    value={formData.amount}
+                    onChange={(e) =>
+                      setFormData({
+                        ...formData,
+                        amount: e.target.value,
+                      })
+                    }
+                  />
+                </Form.Group>
+                <Button variant="primary" type="button">
+                  Send Amount
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card>
+            <Card.Body>
+              <p>Address: {formData.address}</p>
+              <p>Value: {formData.amount}</p>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
